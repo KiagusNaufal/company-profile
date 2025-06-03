0037361f05d1c900aeaf185d04935daf5ea4ece0
KiagusNaufal <kiagusnaufal310@gmail.com>
2025-06-03 16:26:48 +0700

    logo cart

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -102,7 +102,7 @@
                         class="flex-1 px-6 py-3 bg-[#04b2f7] hover:bg-[#0388c4] text-white font-medium rounded-lg shadow-md transition-colors flex items-center justify-center">
                          <span class="mr-2">{{ __('show.buy') }}</span>
                          <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
+                              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0020 7.382V6a2 2 0 00-2-2H6a2 2 0 00-2 2v1.382a1 1 0 00.447.842L9 10m6 0v4a2 2 0 11-4 0v-4m4 0H9" />
                          </svg>
                     </a>
                     
